e5ab2f0b3222dc33a3aed0a5b22d0b28871d5606
765018-stack <765018@student.dusd.net>
2026-05-06 11:41:03 -0700

    worekinmg

--- P4LeonUnit/Assets/Scripts/Enemy.cs
+++ P4LeonUnit/Assets/Scripts/Enemy.cs
@@ -5,7 +5,7 @@ public class Enemy : MonoBehaviour
     public float speed = 3.0f;
     private Rigidbody enemyRb;
     private GameObject player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first frame Update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
@@ -18,5 +18,10 @@ public class Enemy : MonoBehaviour
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
 
         enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
